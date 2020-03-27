11ad080000b87e71637f7c693fbd0709e492d38f
brunoM <bruno.emedeiros99@gmail.com>
2020-03-27 20:25:00 -0300

    versao_03

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyparser = require('body-parser');
-const fs = require('fs').promises;
+const AWS = require('aws-sdk');
 const app = express();
-var campos = {
+const campos = {
     table: []
 };
-
 app.use(express.json());
 app.use(bodyparser.json());
 
-app.post('/posts', function(req, res)
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    Bucket: process.env.AWS_DEFAULT_REGION
+});
+
+app.post('/posts', async (req, res) =>
 {
-   campos.table.push(req.body);
-    const a =  async() => {await fs.writeFile ("input.json", JSON.stringify(campos.table), function(err) {
-        if (err)
-        {
-        console.log(err);
-        }
-        else
-        console.log('Teste');
+        campos.table.push(req.body);
+        
+        var params = {Bucket: 'uploadqrcode', Key: 'users.json', Body: JSON.stringify(campos.table),
+        ContentType: "application/json",
+        ACL: "public-read"};
+
+        s3.upload(params, function(err, data) {
+            if (err) {
+                throw err;
             }
-        );
-    }
-    return res.send(a());
+            return res.send(`File uploaded successfully. ${data.Location}`);
+        });
+        /*
+        await s3.putObject({
+            Bucket: 'uploadqrcode',
+            Key: 'users.json',
+            Body: JSON.stringify(campos.table),
+            ContentType: "application/json",
+            ACL: "public-read"
+            },
+            function (err, data) {
+              console.log(data);
+            }
+          );
+
+          await s3.getSignedUrl('getObject', params, function (err, url) {
+            return res.send(url);
+          });
+          */
 })
 
-app.listen(3000, function()
+
+app.listen(3333, function()
 {
     console.log("Servidor rodando com express");
 });
+
+
+
+
+
+
+
+
+
